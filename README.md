926cf3b53de5754b5aa162931efa272d2f4362e9
Matias <matias.franco289@gmail.com>
2023-06-30 14:43:44 -0300

    Cambiado nombre de variables por advertencias

--- README.md
+++ README.md
@@ -20,7 +20,19 @@ llamado ".env". Dentro de este hay que definir los siguientes parametros.
 Este programa esta desarrollado con python. Para ejecutarlo necesitas
 tener instalado python en tu equipo.
 
-Este programa hace uso de los siguientes paquetes
+Este programa hace uso de los siguientes modulos:
+
+- os
+- sys
+- time
+- threading
+- psycopg2
+- colorama
+- datetime
+- dotenv
+
+Si no tienes instalado alguno de estos modulos puedes instalarlo
+utilizando el comando **pip install "*moduleName*"**.
 
 ## Uso
 
@@ -30,4 +42,13 @@ de dos formas:
 - Desde un editor de codigo puedes ejecutar el archivo **main.py**
 - Desde una consola parado en la carpeta principal puedes usar el comando **python3 main.py** (Ten en cuenta que el comando puede cambiar dependiendo de la version de python instalada).
 
+Una vez ejecutado el programa intentara conectarse a la Base de datos definida en 
+el archivo .env, creara un orden de refresco para las vistas materializadas y
+te lo mostrara por consola.
 
+Se te pedira una confirmacion y en caso de que esta sera positiva("y"), se porcedera
+a refrescar las vistas materializadas en el orden mostrado anteriormente.
+
+Este programa tambien genera un archivo de salida llamado "log.txt" en la ruta principal.
+En este archivo se mostrara la fecha de refresco, los nombres de las vistas refrescadas y su
+cantidad de registros antes y despues de refrescarla.
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ load_dotenv()
 colorama.init()
 
 timerFlag = False
-timerThread = None
+timerThread = threading.Thread()
 matViewsName = set()
 matViewsData = []
 refreshOrder = []
@@ -57,7 +57,7 @@ else:
     print(Fore.GREEN + "Conexion establecida correctamente.")
 
 
-def getTableNames():
+def gettablenames():
     global tableNames
 
     print(Fore.WHITE + "Obteniendo informacion de tablas ..")
@@ -66,17 +66,17 @@ def getTableNames():
         cursor.execute("SELECT tablename FROM pg_tables WHERE schemaname='public'")
         tables = cursor.fetchall()
         tableNames = [''.join(str(elemento) for elemento in tupla) for tupla in tables]
-    except Exception as e:
+    except Exception as tableNameException:
         print(Fore.RED + "No se puedo obtener la informacion de las tablas.")
         print("La siguiente excepcion ha ocurrido: ")
         print("")
-        print(e)
+        print(tableNameException)
         sys.exit()
     else:
         print(Fore.GREEN + "Informacion obtenida correctamente.")
 
 
-def getMatViewsDefinition():
+def getmatviewsdefinition():
     # Obtengo el codigo fuente de todas las vistas materializadas
     global matViewsData, matViewsName
 
@@ -85,11 +85,11 @@ def getMatViewsDefinition():
     try:
         cursor.execute("SELECT matviewname FROM pg_matviews")
         matviews = cursor.fetchall()
-    except Exception as e:
+    except Exception as matViewException:
         print(Fore.RED + "No se pudo obtener la informacion de las vistas materializadas.")
         print("La siguiente excepcion ha ocurrido: ")
         print("")
-        print(e)
+        print(matViewException)
         sys.exit()
     else:
         print(Fore.GREEN + "Informacion obtenida correctamente.")
@@ -97,78 +97,79 @@ def getMatViewsDefinition():
     print(Fore.WHITE + "Obteniendo codigo fuente de vistas materializadas ..")
 
     for matview in matviews:
-        matViewName = matview[0]
-        matViewsName.add(matViewName)
+        matviewname = matview[0]
+        matViewsName.add(matviewname)
         try:
-            cursor.execute(f"SELECT definition FROM pg_matviews WHERE matviewname = '{matViewName}'")
-            matViewDefinition = cursor.fetchone()[0]
-        except Exception as e:
-            print(Fore.RED + f"No se puedo obtener el codigo fuente para la vista materializada {matViewName}.")
+            cursor.execute(f"SELECT definition FROM pg_matviews WHERE matviewname = '{matviewname}'")
+            matviewdefinition = cursor.fetchone()[0]
+        except Exception as fontCodeException:
+            print(Fore.RED + f"No se puedo obtener el codigo fuente para la vista materializada {matviewname}.")
             print("La siguiente excepcion ha ocurrido: ")
             print("")
-            print(e)
+            print(fontCodeException)
             sys.exit()
 
         matViewsData.append({
-            "name": matViewName,
-            "definition": matViewDefinition,
+            "name": matviewname,
+            "definition": matviewdefinition,
             "dependencies": set()
         })
 
     print(Fore.GREEN + "Codigo fuente obtenido correctamente.")
 
 
-def sanitizeWord(word):
+def sanitizeword(word):
     # Eliminar caracteres demas en palabras
-    undesiredCharacters = ["(", ")", ";"]
+    undesiredcharacters = ["(", ")", ";"]
 
-    for character in undesiredCharacters:
+    for character in undesiredcharacters:
         word = word.replace(character, "")
 
-    if word != "": return word
+    if word != "":
+        return word
 
 
-def getMatViewsDependencies():
+def getmatviewsdependencies():
     global matViewsData
     keyword = ["from", "join", "FROM", "JOIN"]
-    undesiredDependencies = []
+    undesireddependencies = []
 
     # Por cada vista
     for matview in matViewsData:
-        viewWords = matview["definition"].split()
+        viewwords = matview["definition"].split()
 
         # Por cada palabras en su codigo fuente
-        for i in range(len(viewWords)):
-            actualWord = viewWords[i]
+        for i in range(len(viewwords)):
+            actualword = viewwords[i]
             # Cualquier palabra despues de un FROM o JOIN no vacia es agregada como dependencia
-            if keyword.__contains__(actualWord):
-                nextWord = sanitizeWord(viewWords[i + 1])
-                if nextWord is not None:
-                    matview["dependencies"].add(nextWord)
+            if keyword.__contains__(actualword):
+                nextword = sanitizeword(viewwords[i + 1])
+                if nextword is not None:
+                    matview["dependencies"].add(nextword)
 
             # Como se agregan todas las palabras despues de un FROM puede ser que esa dependencia
             # Sea una dependencia agregada con la clausula WITH en cuyo caso no me interesaria porque
             # No es ni una tabla ni una vista ni una vista materializada
             # Asi que las agrego a una lista para depues poder filtrarlas y eliminarlas
-            if(actualWord == "WITH"):
-                undesiredDependencies.append(sanitizeWord(viewWords[i + 1]))
+            if actualword == "WITH":
+                undesireddependencies.append(sanitizeword(viewwords[i + 1]))
 
         # Filtro y elimino dependencias agregadas por WITH
-        matview["dependencies"] = tuple(x for x in matview["dependencies"] if not undesiredDependencies.__contains__(x))
+        matview["dependencies"] = tuple(x for x in matview["dependencies"] if not undesireddependencies.__contains__(x))
 
 
-def isDependency(usedDependencies, validDependencies):
+def isdependency(useddependencies, validdependencies):
     # Esta funcion determina si las dependencias de una vista se encuentran en la lista de dependencias validas
 
-    for dependency in usedDependencies:
-        if not validDependencies.__contains__(dependency) and not tableNames.__contains__(dependency):
+    for dependency in useddependencies:
+        if not validdependencies.__contains__(dependency) and not tableNames.__contains__(dependency):
             return False
     return True
 
 
-def setMatViewsPriority():
+def setmatviewspriority():
     global refreshOrder
-    totalViewsAmount = len(matViewsData)
+    totalviewsamount = len(matViewsData)
 
     print("")
     print(Fore.WHITE + "Estableciendo orden de refresco ..")
@@ -177,7 +178,7 @@ def setMatViewsPriority():
     i = 0
     while i < len(matViewsData):
         actualmatview = matViewsData[i]
-        if isDependency(actualmatview["dependencies"], refreshOrder):
+        if isdependency(actualmatview["dependencies"], refreshOrder):
             refreshOrder.append(actualmatview["name"])
             print(actualmatview["name"])
             del matViewsData[i]
@@ -186,78 +187,80 @@ def setMatViewsPriority():
 
     print("")
     print(
-        Fore.GREEN + f"Orden establecido para un total de {len(refreshOrder)} de {totalViewsAmount} vistas materializadas.")
+        Fore.GREEN + f"Orden establecido para un total de "
+                     f"{len(refreshOrder)} de {totalviewsamount} vistas materializadas.")
 
-    if totalViewsAmount != len(refreshOrder):
+    if totalviewsamount != len(refreshOrder):
         print(Fore.RED + f"No se pudo establecer el orden para un total de {len(matViewsData)} vistas materializadas.")
         print("")
         for view in matViewsData:
             print(view["name"])
 
 
-def checkUserConfirmation(question):
-    validRespones = ["Y", "N"]
+def checkuserconfirmation(question):
+    validrespones = ["Y", "N"]
     print(Fore.WHITE + "----------------")
     print("")
-    userResponse = input(question).upper()
+    userresponse = input(question).upper()
 
-    while not validRespones.__contains__(userResponse):
+    while not validrespones.__contains__(userresponse):
         print(Fore.RED + "Resupuesta no valida. Introduza Y para confirmar o N para negar.")
-        userResponse = input().upper()
+        userresponse = input().upper()
 
-    if userResponse != "Y":
+    if userresponse != "Y":
         sys.exit()
 
 
-def startTimer():
+def starttimer():
     global timerFlag, timerThread
     timerFlag = True
     timerThread = threading.Thread(target=timer)
     timerThread.start()
 
 
-def stopTimer():
+def stoptimer():
     global timerFlag, timerThread
     timerFlag = False
     timerThread.join()
 
-def getMatViewCount(matviewName):
+
+def getmatviewcount(matviewname):
     try:
-        query = f'SELECT COUNT(*) FROM "{matviewName}";'
+        query = f'SELECT COUNT(*) FROM "{matviewname}";'
         cursor.execute(query)
-        viewRecordsAmount = cursor.fetchone()[0]
-        return viewRecordsAmount
-    except Exception as e:
-        return "ERROR"
+        viewrecordsamount = cursor.fetchone()[0]
+        return viewrecordsamount
+    except Exception as matViewCountException:
+        return matViewCountException
 
 
-def refreshAll():
+def refreshall():
     for view in refreshOrder:
 
         logger(["", "---"])
         logger([view])
         logger(["---"])
-        logger([f"Cantidad de registros anterior: {getMatViewCount(view)}"])
+        logger([f"Cantidad de registros anterior: {getmatviewcount(view)}"])
 
         query = f'REFRESH MATERIALIZED VIEW "{view}";'
         print(Fore.WHITE + "Resfrescando vista materializada " + Fore.CYAN + f"{view}" + Fore.WHITE + ".")
-        startTimer()
+        starttimer()
 
         try:
             cursor.execute(query)
             conn.commit()
-        except Exception as e:
-            stopTimer()
+        except Exception as refreshException:
+            stoptimer()
             print(Fore.RED + f"No se pudo refrescar {view}.")
             print("La siguiente excepcion ha ocurrido mientras se intentaba refrescar: ")
             print("")
-            print(e)
+            print(refreshException)
             sys.exit()
         else:
-            stopTimer()
+            stoptimer()
             print(Fore.CYAN + f"{view} " + Fore.GREEN + "refrescada correctamente.")
             print("")
-            logger([f"Cantidad de registros actual: {getMatViewCount(view)}"])
+            logger([f"Cantidad de registros actual: {getmatviewcount(view)}"])
 
         logger(["---", ""])
 
@@ -285,13 +288,13 @@ def logger(messages):
                 file.write(message + '\n')
 
 
-getTableNames()
-getMatViewsDefinition()
-getMatViewsDependencies()
-setMatViewsPriority()
-checkUserConfirmation("Esta seguro que desea refrescar estas vistas materializadas? [Y,N]: ")
+gettablenames()
+getmatviewsdefinition()
+getmatviewsdependencies()
+setmatviewspriority()
+checkuserconfirmation("Esta seguro que desea refrescar estas vistas materializadas? [Y,N]: ")
 logger(["----------", f"{datetime.now().strftime('%d-%m-%Y')} {datetime.now().strftime('%H:%M:%S')}", "----------"])
-refreshAll()
+refreshall()
 logger(["", ""])
 print("")
 conn.close()
